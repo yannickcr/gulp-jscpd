f21689f98b1018653c09a064bb5563e3908743c7
Yannick Croissant <yannick.croissant@gmail.com>
2014-06-15 01:02:58 +0200

    First commit

---
+++ test/test.js
@@ -0,0 +1,11 @@
+/* jshint expr:true */
+'use strict';
+
+var chai    = require('chai');
+var expect  = chai.expect;
+
+describe('gulp-jscpd', function() {
+  it('must be tested', function() {
+    expect(true).to.be.true;
+  });
+});
